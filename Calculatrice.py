04abbc2b1f0cd1ec66078e16c265c555ffb4bd2e
AlDuelz <79324894+AlDuelz@users.noreply.github.com>
2021-06-19 19:59:23 +0200

    Ma calculatrice
    Je suis encore un débutant en Python. (J'ai commencé hier)

---
+++ Calculatrice.py
@@ -0,0 +1,45 @@
+import os
+
+def Addition(x, y):
+    z = x + y
+    print(f"La reponse est {z}")
+
+
+def Soustraction(x, y):
+    z = x - y 
+    print(f"La reponse est {z}")
+
+def Multiplication(x, y):
+    z = x * y
+    print(f"La reponse est {z}")
+
+def Division (x, y):
+    z = x / y 
+    print(f"La reponse est {z}")
+
+try: 
+    nombre1 = int(input("Quel est le premier nombre que tu veux ?: "))
+    nombre2 = int(input("Quel est le deuxième nombre que tu veux?: "))
+    print("Que veux-tu faire avec les nombres?:\n ")
+    print("  [1] Je veux une Addition !\n")
+    print("  [2] Je veux une Soustraction !\n")
+    print("  [3] Je veux une Multiplication !\n")
+    print("  [4] Je veux une Division !\n")
+    operator = int(input(">: "))
+
+    if (operator == 1):
+        Addition(nombre1, nombre2)
+    elif (operator == 2):
+        Soustraction(nombre1, nombre2)
+    elif (operator == 3):
+        Multiplication(nombre1, nombre2)
+    elif (operator == 4):
+        Division(nombre1, nombre2)
+
+    else: 
+        print("Les seules possibilitées sont 1, 2, 3 et 4.")
+
+except ValueError:
+    print("Alors frerot, on ne sait pas mettre une valeur ?")
+except:
+    print("Une erreur inconnue s'est produite")
